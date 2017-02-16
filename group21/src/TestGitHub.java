35f2da8d3546be79173d59c9ddf50ee9eb9a371c
Stevenliyijun <stevenliyijun@163.com>
2017-02-17 06:42:38 +0800

    Rejected commit: Default

---
+++ group21/src/TestGitHub.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/2/17 0017.
+ */
+public class TestGitHub {
+
+    public static void main(String args[]) {
+
+        System.out.println("First commit.");
+
+    }
+}
